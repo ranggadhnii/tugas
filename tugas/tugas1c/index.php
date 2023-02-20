871d39f805d8ff88aab68240a8b6ad46ae118063
ranggadhnii <125874628+ranggadhnii@users.noreply.github.com>
2023-02-20 18:00:28 +0700

    Add files via upload

---
+++ tugas/tugas1c/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 1c</title>
+</head>
+<body>
+    <style>
+
+        table {
+            border-width: 0px;
+            border-collapse: collapse;
+        }
+
+        td {
+            border: 2px solid;
+            text-align: center;
+            padding: 30px 30px 30px 30px;
+            background-color: grey;
+            font-family: sans-serif;
+            font-size: 25px;
+        }
+        
+    </style>
+
+    <table boder="1" cellpadding="10">
+        <tr>
+            <td>1</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>2</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>3</td>
+            <td>3</td>
+        </tr>
+    </table>
+</body>
+</html>
